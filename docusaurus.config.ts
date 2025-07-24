b40866383449ae92673013599f0a9dbac52a44cb
Thomas CILES <36271202+tciles@users.noreply.github.com>
2025-07-24 11:30:29 +0200

    fix : url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://tciles.github.io/documentation/',
+  url: 'https://tciles.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/documentation/',
